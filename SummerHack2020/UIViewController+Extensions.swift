97da926b84134fbfd0c02658012ad92b4f7b9d24
chiukelly <chiukelly3@gmail.com>
2020-06-16 02:29:12 -0500

    added create account screen and transition to main app woohoo

---
+++ SummerHack2020/UIViewController+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Extensions.swift
+//  SummerHack2020
+//
+//  Created by Kelly Chiu on 6/16/20.
+//  Copyright © 2020 momma wang and children. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {}
+}
