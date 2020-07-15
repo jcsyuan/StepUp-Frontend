3a9573f79a6193a24ba5037a7b40c419d4564050
Kelly Chiu <chiukelly3@gmail.com>
2020-07-15 14:52:21 -0500

    kelly's stuff again

--- SummerHack2020/ViewController.swift
+++ SummerHack2020/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var stepsLabel: UILabel!
+    var val = 0
+    let healthStore = HKHealthStore()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        authorizeHealthKit()
+    }
+    
+    func authorizeHealthKit(){
+        let read = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!])
+        let share = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!])
+        healthStore.requestAuthorization(toShare: share, read: read) { (chk, error) in
+            if(chk) {
+                print("permission granted")
+                self.getTodayTotalStepCounts()
+            }
+        }
     }
 
+    func getTodayTotalStepCounts() {
+        guard let sampleType = HKObjectType.quantityType(forIdentifier: .stepCount) else {
+            return
+        }
+        let startDate = Calendar.current.startOfDay(for: Date())
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: .strictEndDate)
+        var interval = DateComponents()
+     
+        // not sure lol
+        interval.day = 7
+        let query = HKStatisticsCollectionQuery(quantityType: sampleType, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: startDate, intervalComponents: interval)
+        query.initialResultsHandler = {
+            query,result,error in
 
+            if let myresult = result {
+                myresult.enumerateStatistics(from: startDate, to: Date()) { (statistic, value) in
+                    if let count = statistic.sumQuantity() {
+                        self.val = Int(count.doubleValue(for: HKUnit.count()))
+                        print("Total step taken today is \(self.val) steps")
+                     
+                    }
+                }
+            }
+        }
+        healthStore.execute(query)
+    }
+    
+    @IBAction func reloadSteps(_ sender: Any) {
+        getTodayTotalStepCounts()
+        stepsLabel.text = String(val)
+    }
 }
 
