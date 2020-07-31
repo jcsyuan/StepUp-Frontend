a37d2cd427512264530c2ba4e027dedbd2954957
jcsyuan <jonjonyuan2010@gmail.com>
2020-07-31 15:57:36 -0500

    json initialize account 2.0

--- SummerHack2020/createAccountViewController.swift
+++ SummerHack2020/createAccountViewController.swift
@@ -9,26 +9,25 @@
 import UIKit
 
 class createAccountViewController: UIViewController {
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-          // json stuff
+        // json stuff
         let url = URL(string: "http://127.0.0.1:5000/initialize-account")!
         var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
-                request.httpMethod = "POST"
-        request.multipartFormData(parameters: ["username": "gn", "password": "secret", "display_name": "grace", "coins": "1"])
+        request.httpMethod = "POST"
+        request.multipartFormData(parameters: ["username": "gn", "password": "secret", "display_name": "grace", "email": "grac@gmail.com", "coins": "1"])
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data else { return }
-                do {
-                  print()
-                } catch let jsonErr {
-                        print(jsonErr)
-                    }
-                }
-        //        print(nameArray)
-                task.resume()
+            do {
+                print()
+            } catch let jsonErr {
+                print(jsonErr)
+            }
+        }
+        task.resume()
     }
 }
 
