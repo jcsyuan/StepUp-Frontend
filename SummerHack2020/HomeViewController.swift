e90227b0c66c01e588384a2d7749648911d80e06
Kelly Chiu <chiukelly3@gmail.com>
2020-08-01 12:48:54 -0500

    login

---
+++ SummerHack2020/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  SummerHack2020
+//
+//  Created by Kelly Chiu on 8/1/20.
+//  Copyright © 2020 momma wang and children. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SummerHack2020/SignInViewController.swift
+++ SummerHack2020/SignInViewController.swift
@@ -14,6 +14,49 @@ struct defaultsKeys {
 }
 
 class SignInViewController: UIViewController {
+    var usernameText: String = ""
+    var passwordText: String = ""
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var `continue`: UIButton!
+    
+    @IBAction func getVal () {
+        usernameText = username.text!
+        passwordText = password.text!
+        
+        let url = URL(string: "http://127.0.0.1:5000/login")!
+               var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
+               request.httpMethod = "POST"
+               request.multipartFormData(parameters: ["username": usernameText, "password": passwordText])
+               let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                   guard let data = data else { return }
+                   do {
+                       let tempCredentials = try JSONDecoder().decode(loginCredentials.self, from: data)
+                       self.user_id = tempCredentials.user_id
+                       self.token = tempCredentials.token
+                   } catch let jsonErr {
+                       print(jsonErr)
+                       }
+                   }
+               //
+               task.resume()
+        
+        //alert message
+        if(self.user_id == 0) {
+                  print("HI")
+                  alert(message: "The username or password you entered is not valid", title: "LOGIN FAILED")
+              } else {
+                
+                
+                  // set defaults with web endpoint data
+                  let defaults = UserDefaults.standard
+                  defaults.set(self.user_id, forKey: defaultsKeys.userIdKey)
+                  defaults.set(self.token, forKey: defaultsKeys.tokenKey)
+              }
+    }
+    
     struct loginCredentials: Codable{
         let user_id: Int
         let token: Int
@@ -24,22 +67,7 @@ class SignInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "http://127.0.0.1:5000/login")!
-        var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
-        request.httpMethod = "POST"
-        request.multipartFormData(parameters: ["username": "pikchiu", "password": "secre"])
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard let data = data else { return }
-            do {
-                let tempCredentials = try JSONDecoder().decode(loginCredentials.self, from: data)
-                self.user_id = tempCredentials.user_id
-                self.token = tempCredentials.token
-            } catch let jsonErr {
-                print(jsonErr)
-                }
-            }
-        //
-        task.resume()
+       
         
         
         // set defaults with web endpoint data
@@ -48,19 +76,6 @@ class SignInViewController: UIViewController {
 //        let defaults = UserDefaults.standard
 //        if let user_id = defaults.string(forKey: defaultsKeys.userIdKey)
 //        if let token = defaults.string(forKey: defaultsKeys.tokenKey)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        if(self.user_id == 0) {
-            print("HI")
-            alert(message: "The username or password you entered is not valid", title: "LOGIN FAILED")
-        } else {
-            // set defaults with web endpoint data
-            let defaults = UserDefaults.standard
-            defaults.set(self.user_id, forKey: defaultsKeys.userIdKey)
-            defaults.set(self.token, forKey: defaultsKeys.tokenKey)
-        }
-        
     }
 }
 
