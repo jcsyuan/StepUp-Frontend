51623b8b570f7fd3bbe1b9e9657bfae28a919b16
jcsyuan <jonjonyuan2010@gmail.com>
2020-08-02 19:56:33 -0500

    login successful

--- SummerHack2020/SignInViewController.swift
+++ SummerHack2020/SignInViewController.swift
@@ -14,69 +14,8 @@ struct defaultsKeys {
 }
 
 class SignInViewController: UIViewController {
-    var usernameText: String = ""
-    var passwordText: String = ""
     
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var password: UITextField!
-    
-    @IBOutlet weak var `continue`: UIButton!
-    
-    @IBAction func getVal () {
-        
-        let operation1 = BlockOperation {
-            let group = DispatchGroup()
-            
-            group.enter()
-        
-            self.usernameText = self.username.text!
-            self.passwordText = self.password.text!
-            
-            let url = URL(string: "http://127.0.0.1:5000/login")!
-            var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
-            request.httpMethod = "POST"
-            request.multipartFormData(parameters: ["username": self.usernameText, "password": self.passwordText])
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                guard let data = data else { return }
-                do {
-                    let tempCredentials = try JSONDecoder().decode(loginCredentials.self, from: data)
-                    DispatchQueue.main.async {
-                        self.user_id = tempCredentials.user_id
-                        self.token = tempCredentials.token
-                    }
-                } catch let jsonErr {
-                    print(jsonErr)
-                }
-            }
-            //
-            task.resume()
-            group.leave()
-            group.wait()
-        }
-        
-        
-        //alert message
-        let operation2 = BlockOperation {
-            print(self.user_id)
-            if(self.user_id == 0) {
-                self.alert(message: "The username or password you entered is not valid", title: "LOGIN FAILED")
-            } else {
-                // set defaults with web endpoint data
-                let defaults = UserDefaults.standard
-                defaults.set(self.user_id, forKey: defaultsKeys.userIdKey)
-                defaults.set(self.token, forKey: defaultsKeys.tokenKey)
-                self.performSegue(withIdentifier: "successfulLogin", sender: self)
-            }
-        }
-        operation2.addDependency(operation1)
-        
-        let radQueue = OperationQueue()
-        radQueue.addOperation(operation1)
-        radQueue.addOperation(operation2)
-        
-    }
-    
-    struct loginCredentials: Codable{
+    struct loginCredentials: Codable {
         let user_id: Int
         let token: String
     }
@@ -84,23 +23,62 @@ class SignInViewController: UIViewController {
     var user_id: Int = 0
     var token: String = ""
     
+    var usernameText: String = ""
+    var passwordText: String = ""
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    @IBAction func continueAction(_ sender: Any) {
+        self.usernameText = self.username.text!
+        self.passwordText = self.password.text!
+        
+        let url = URL(string: "http://127.0.0.1:5000/login")!
+        var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
+        request.httpMethod = "POST"
+        request.multipartFormData(parameters: ["username": self.usernameText, "password": self.passwordText])
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
+            do {
+                let tempCredentials = try JSONDecoder().decode(loginCredentials.self, from: data)
+                DispatchQueue.main.async {
+                    self.user_id = tempCredentials.user_id
+                    self.token = tempCredentials.token
+                    print(self.user_id)
+                    if(self.user_id == 0) {
+                        print("wrong")
+                        // present alert
+                        let message: String = "The username or password you entered is not valid"
+                        let titleText: String = "LOGIN FAILED"
+                        let alertController:UIAlertController = UIAlertController(title: titleText, message: message, preferredStyle: UIAlertController.Style.alert)
+                        let alertAction:UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:nil)
+                        alertController.addAction(alertAction)
+                        self.present(alertController, animated: true, completion: nil)
+                    } else {
+                        print("right")
+                        // segue to next viewcontroller
+                        let defaults = UserDefaults.standard
+                        defaults.set(self.user_id, forKey: defaultsKeys.userIdKey)
+                        defaults.set(self.token, forKey: defaultsKeys.tokenKey)
+                        self.performSegue(withIdentifier: "presentHome", sender: self)
+                    }
+                }
+            } catch let jsonErr {
+                print(jsonErr)
+            }
+        }
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // set defaults with web endpoint data
         
         // get defaults data
-        //        let defaults = UserDefaults.standard
-        //        if let user_id = defaults.string(forKey: defaultsKeys.userIdKey)
-        //        if let token = defaults.string(forKey: defaultsKeys.tokenKey)
-    }
-}
-
-extension SignInViewController {
-    func alert(message: String, title: String) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(OKAction)
-        self.present(alertController, animated: true, completion: nil)
+        // let defaults = UserDefaults.standard
+        // if let user_id = defaults.string(forKey: defaultsKeys.userIdKey)
+        // if let token = defaults.string(forKey: defaultsKeys.tokenKey)
     }
 }
